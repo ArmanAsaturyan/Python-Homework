1524a6ad2779cd755b47efdc1a2565fee7814ef5
ArmanAsaturyan <124360777+ArmanAsaturyan@users.noreply.github.com>
2023-08-29 21:59:52 +0400

    Add files via upload

---
+++ Homework 1.py
@@ -0,0 +1,29 @@
+
+1.
+kg = int(input('Enter kg: '))
+print(f'{kg} Kg = {kg * 2.2} Pounds')
+
+
+2.
+import random
+for i in range(50):
+    print(random.randint(3,6) , end = ',')
+
+3.
+x = int(input('Enter the first number: '))
+y = int(input('Enter the second number: '))
+print(abs(x - y) / (x + y))
+
+
+4.
+x = int(input('Enter the first number: '))
+y = int(input('Enter the second number: '))
+z = int(input('Enter the tirst number: '))
+print(f'Sum of numbers = {x + y + z}')
+print(f'Average = {(x + y + z) / 3}')
+
+
+5.
+for i in range(1,5):
+    x = '*' * i
+    print(x)
